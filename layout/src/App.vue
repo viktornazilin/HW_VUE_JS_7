7c14616e54121b73dddca4032a2af31a6cbe9f04
viktornazilin <104361827+viktornazilin@users.noreply.github.com>
2023-11-16 19:30:25 +0100

    Add files via upload

---
+++ layout/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <ProjectDetails />
+
+    <ProjectPage />
+
+    <HomePage />
+
+    <BlogPage />
+
+    <BlogDetailsPage />
+  </div>
+</template>
+
+<script>
+
+import HomePage from './pages/HomePage.vue'
+
+import BlogPage from './pages/BlogPage.vue'
+
+import BlogDetailsPage from './pages/BlogDetailsPage.vue'
+
+import ProjectPage from './pages/ProjectPage.vue'
+
+import ProjectDetails from './pages/ProjectDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+
+    BlogPage,
+
+    BlogDetailsPage,
+
+    ProjectPage,
+
+    ProjectDetails
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "scss/vars";
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - $widthSite / 2);
+  padding-right: calc(50% - $widthSite / 2);
+}
+</style>
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Домашнее задание к семинару 7
+
+Установите VueX и добавьте к проекту.
+
+Приступаем к вёрстке новой страницы [project details](https://www.figma.com/file/okdYD45Tj2JpKsNASccUmf/Interior-Design-Webflow-Website-Template-(Community)-(Copy)-(Copy)?type=design&node-id=1418-15&mode=design&t=ebkazpiG5BOth8x7-0) (Домашнее задание 7):
+
+Информацию про описание проекта вынести в state.
+
+* Слайдер можно сделать самостоятельно с помощью vue или сделать с помощью bootstrap.
+
+Задание по работе с макетом направлено на подготовку проекта к итоговой аттестации.
